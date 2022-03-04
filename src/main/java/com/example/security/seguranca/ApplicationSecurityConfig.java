f1f07a5f4d1e9e469796c74bae1fefd09c9edad7
araujogabriel00 <gabriel.araujos@sempreceub.com>
2022-03-04 18:51:19 -0300

    Password ecoding with bcrypt

--- src/main/java/com/example/security/seguranca/ApplicationSecurityConfig.java
+++ src/main/java/com/example/security/seguranca/ApplicationSecurityConfig.java
@@ -1,15 +1,28 @@
 package com.example.security.seguranca;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 //BASIC AUTH
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
 
     @Override
@@ -22,5 +35,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()//authenticated username and password
                 .and()
                 .httpBasic();//mecanism to effort the autentication
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails gabrielAraujo = User.builder()
+                .username("Gabriel")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT") //ROLE_STUDENT
+                .build();
+        return new InMemoryUserDetailsManager(
+                gabrielAraujo
+        );
     }
 }
